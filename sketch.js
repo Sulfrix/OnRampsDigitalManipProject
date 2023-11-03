d58d6a047b0a5db43ba14af1e6c2edef4001bf15
Sulfrix <deucebroom@outlook.com>
2023-11-03 08:59:24 -0500

    remove all logic and data relating to parts

--- sketch.js
+++ sketch.js
@@ -1,41 +1,24 @@
 /// <reference path="types/p5/global.d.ts"/>
 
-
 let workspace;
 
 let canvas;
 
-let categories;
-
-let helpEle;
-
-let sounds;
-
-let music;
-
-let musicPlaying = false;
-
 function preload() {
-  categories = loadJSON("categories.json");
 }
 
 function setup() {
   // put setup code here
   canvas = createCanvas(windowWidth, windowHeight);
 
-  workspace = new FrontEndWorkspace();
+  workspace = new Workspace();
   pInput.registerCanvas(canvas);
   pInput.registerWorkspace(workspace);
-  workspace.addTool(new HandTool());
-  workspace.selectTool(workspace.tools[0]);
   //music.volume(0.2);
 }
 
 function mouseClicked() {
-  /*if (!musicPlaying) {
-    music.loop();
-    musicPlaying = true;
-  }*/
+
 }
 
 function draw() {
@@ -48,20 +31,3 @@ function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
   workspace.cacheMouseArea = null;
 }
-
-function openHelp() {
-  if (!helpEle) {
-    helpEle = document.createElement("iframe");
-  helpEle.src = "helppage.html";
-  document.body.appendChild(helpEle);
-  }
-}
-
-window.addEventListener("message", (e) => {
-  if (e.data == "closeMePlease") {
-    helpEle.remove();
-    helpEle = null;
-  }
-})
-
-let audioCache = {};
