f1f48a614d5be7ec52c38fb0ecd713ea6563fe33
Sulfrix <deucebroom@outlook.com>
2023-11-07 14:14:03 -0600

    lay foundations for nodes

--- sketch.js
+++ sketch.js
@@ -10,10 +10,14 @@ function preload() {
 function setup() {
   // put setup code here
   canvas = createCanvas(windowWidth, windowHeight);
+  window.p5Canvas = canvas
 
   workspace = new Workspace();
   pInput.registerCanvas(canvas);
   pInput.registerWorkspace(workspace);
+  workspace.addNode(new PNode())
+  workspace.addNode(new PNode())
+  workspace.nodes[1].pos.x = 200
   //music.volume(0.2);
 }
 
@@ -22,7 +26,7 @@ function mouseClicked() {
 }
 
 function draw() {
-  background(220);
+  background(51);
   workspace.update();
   workspace.draw();
 }
