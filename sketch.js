c759b2dde77d677126a091d1d1b7682028195a17
Sulfrix <deucebroom@outlook.com>
2023-11-13 08:40:09 -0600

    Experimental selection tool marquee + prettier

--- sketch.js
+++ sketch.js
@@ -4,77 +4,73 @@ let workspace;
 
 let canvas;
 
-function preload() {
-}
+function preload() {}
 
 function setup() {
 	// put setup code here
 	canvas = createCanvas(windowWidth, windowHeight);
-	window.p5Canvas = canvas
-
+	window.p5Canvas = canvas;
 
 	workspace = new Workspace();
 	pInput.registerCanvas(canvas);
 	pInput.registerWorkspace(workspace);
-	workspace.addTool(new SelectionTool())
-	workspace.addTool(new MoveTool())
-	workspace.addTool(new RotateTool())
-	workspace.addTool(new ScaleTool())
-	workspace.selectTool(workspace.tools[0])
+	workspace.addTool(new SelectionTool());
+	workspace.addTool(new MoveTool());
+	workspace.addTool(new RotateTool());
+	workspace.addTool(new ScaleTool());
+	workspace.selectTool(workspace.tools[0]);
 	for (let x = 0; x < 10; x++) {
 		for (let y = 0; y < 10; y++) {
-			let node = new PNode()
-			node.pos.x = x*300
-			node.pos.y = y*300
-			workspace.addNode(node)
+			let node = new PNode();
+			node.pos.x = x * 300;
+			node.pos.y = y * 300;
+			workspace.addNode(node);
 		}
 	}
 	//textFont(arial)
 	//music.volume(0.2);
 }
 
-var frameTimes = []
+var frameTimes = [];
 
-function mouseClicked() {
-
-}
+function mouseClicked() {}
 
 function draw() {
 	background(51);
-	var startTime = performance.now()
+	var startTime = performance.now();
 	workspace.update();
 	workspace.draw();
-	var endTime = performance.now()
-	var frameTime = endTime - startTime
-	frameTimes.push(frameTime)
+	var endTime = performance.now();
+	var frameTime = endTime - startTime;
+	frameTimes.push(frameTime);
 	if (frameTimes.length > 30) {
-		frameTimes.shift()
+		frameTimes.shift();
 	}
-	var sum = 0
+	var sum = 0;
 	for (let t of frameTimes) {
-		sum += t
+		sum += t;
 	}
-	var avgFrameTime = sum/frameTimes.length
-	fill(255)
-	text((Math.round(avgFrameTime*100) / 100) + "ms", 10, 20)
-	perfGraph(avgFrameTime)
+	var avgFrameTime = sum / frameTimes.length;
+	fill(255);
+	text(Math.round(avgFrameTime * 100) / 100 + "ms", 10, 20);
+	perfGraph(avgFrameTime);
 }
 
 function perfGraph(avg) {
-	push()
-	noStroke()
-	fill(255, 128)
-	let s = 4
+	push();
+	noStroke();
+	fill(255, 128);
+	let s = 4;
 	for (let i = 0; i < frameTimes.length; i++) {
-		var t = frameTimes[i]
-		rect(i*s, 0, s, t*10)
+		var t = frameTimes[i];
+		rect(i * s, 0, s, t * 10);
 	}
-	noFill()
-	stroke(255)
-	line(0, avg*10, frameTimes.length*s, avg*10)
-	stroke(255, 0, 0)
-	line(0, 16*10, frameTimes.length*s, 16*10)
-	pop()
+	noFill();
+	stroke(255);
+	line(0, avg * 10, frameTimes.length * s, avg * 10);
+	stroke(255, 0, 0);
+	line(0, 16 * 10, frameTimes.length * s, 16 * 10);
+	pop();
 }
 
 function windowResized() {
