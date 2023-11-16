c678567fe6e73c40e8ba3d5c486d5d34d42d0e2b
Sulfrix <deucebroom@outlook.com>
2023-11-16 08:48:53 -0600

    Perf graph

--- sketch.js
+++ sketch.js
@@ -4,29 +4,42 @@ let workspace;
 
 let canvas;
 
+let perfGraph
+
 function preload() {}
+
+function workspaceTemplate() {
+	return Workspace.newFromTemplate()
+}
+
+function switchWorkspace(wspc) {
+	workspace = wspc;
+	pInput.registerWorkspace(workspace);
+}
 
 function setup() {
 	// put setup code here
 	canvas = createCanvas(windowWidth, windowHeight);
 	window.p5Canvas = canvas;
 
-	workspace = new Workspace();
-	pInput.registerCanvas(canvas);
-	pInput.registerWorkspace(workspace);
-	workspace.addTool(new SelectionTool());
-	workspace.addTool(new MoveTool());
-	workspace.addTool(new RotateTool());
-	workspace.addTool(new ScaleTool());
-	workspace.selectTool(workspace.tools[0]);
-	for (let x = 0; x < 10; x++) {
-		for (let y = 0; y < 10; y++) {
-			let node = new PNode();
-			node.pos.x = x * 300;
-			node.pos.y = y * 300;
-			workspace.addNode(node);
+	workspace = workspaceTemplate()
+	switchWorkspace(workspace)
+	PerfTrack.enableTracking()
+	console.log(PerfTrack.measure("Create Test Nodes", () => {
+		for (let x = 0; x < 10; x++) {
+			for (let y = 0; y < 10; y++) {
+				let node = new PNode();
+				node.pos.x = x * 300;
+				node.pos.y = y * 300;
+				workspace.addNode(node);
+			}
 		}
-	}
+	}))
+	pInput.registerCanvas(canvas);
+	
+	
+	
+	perfGraph = new PerfGraph();
 	//textFont(arial)
 	//music.volume(0.2);
 }
@@ -35,42 +48,39 @@ var frameTimes = [];
 
 function mouseClicked() {}
 
+var debugScale = false
+var prevPerfTrack = null
+
+var displayedTrack = null
+
 function draw() {
+	PerfTrack.push("Main Loop")
+	PerfTrack.push("Work")
 	background(51);
-	var startTime = performance.now();
+	if (debugScale) {
+		scale(0.5)
+		translate(width/2, height/2)
+		push()
+		noFill()
+		stroke(255)
+		rect(0, 0, width, height)
+		pop()
+	}
+	//workspace.screenArea.rot += 0.08*deltaTime/1000
 	workspace.update();
 	workspace.draw();
-	var endTime = performance.now();
-	var frameTime = endTime - startTime;
-	frameTimes.push(frameTime);
-	if (frameTimes.length > 30) {
-		frameTimes.shift();
+	PerfTrack.pop();
+	var loop = PerfTrack.pop()
+	if (perfGraph && loop) {
+		//loop.endTime = loop.startTime + 16
+		perfGraph.recordTime(loop.getLength());
+		perfGraph.drawGraph(true)
+		if (frameCount % 5 == 0 || !displayedTrack) {
+			displayedTrack = loop
+		}
+		displayedTrack.display(50, height-20, width-100, 0)
 	}
-	var sum = 0;
-	for (let t of frameTimes) {
-		sum += t;
-	}
-	var avgFrameTime = sum / frameTimes.length;
-	fill(255);
-	text(Math.round(avgFrameTime * 100) / 100 + "ms", 10, 20);
-	perfGraph(avgFrameTime);
-}
-
-function perfGraph(avg) {
-	push();
-	noStroke();
-	fill(255, 128);
-	let s = 4;
-	for (let i = 0; i < frameTimes.length; i++) {
-		var t = frameTimes[i];
-		rect(i * s, 0, s, t * 10);
-	}
-	noFill();
-	stroke(255);
-	line(0, avg * 10, frameTimes.length * s, avg * 10);
-	stroke(255, 0, 0);
-	line(0, 16 * 10, frameTimes.length * s, 16 * 10);
-	pop();
+	prevPerfTrack = loop
 }
 
 function windowResized() {
