273081dc7bffc36b2713343c4646c8cfbf0ac446
Sulfrix <deucebroom@outlook.com>
2023-11-29 19:53:33 -0600

    improve socket visuals + load/save buttons

--- sketch.js
+++ sketch.js
@@ -4,14 +4,16 @@ let workspace;
 
 let canvas;
 
-let perfGraph
+let perfGraph;
+
+let loadingPromise;
 
 function preload() {
-	NodeDef.loadNodeDefs()
+	loadingPromise = NodeDef.loadNodeDefs();
 }
 
 function workspaceTemplate() {
-	return Workspace.newFromTemplate()
+	return Workspace.newFromTemplate();
 }
 
 function switchWorkspace(wspc) {
@@ -24,23 +26,28 @@ function setup() {
 	canvas = createCanvas(windowWidth, windowHeight);
 	window.p5Canvas = canvas;
 
-	workspace = workspaceTemplate()
-	switchWorkspace(workspace)
-	//PerfTrack.enableTracking()
-	console.log(PerfTrack.measure("Create Test Nodes", () => {
-		for (let x = 0; x < 0; x++) {
-			for (let y = 0; y < 0; y++) {
-				let node = new PNode();
-				node.pos.x = x * 300;
-				node.pos.y = y * 300;
-				workspace.addNode(node);
+	workspace = workspaceTemplate();
+	switchWorkspace(workspace);
+	PerfTrack.enableTracking();
+
+	let testnodes = () => {
+		let testnodesperf = PerfTrack.measure("Create Test Nodes", () => {
+			for (let x = 0; x < 10; x++) {
+				for (let y = 0; y < 10; y++) {
+					let node = NodeDef.createFromPath("math/divide");
+					node.pos.x = x * 300;
+					node.pos.y = y * 300;
+					workspace.addNode(node);
+				}
 			}
+		});
+		if (PerfTrack.enabled) {
+			console.log(testnodesperf);
 		}
-	}))
+	};
+	loadingPromise.then(testnodes)
 	pInput.registerCanvas(canvas);
-	
-	
-	
+
 	perfGraph = new PerfGraph();
 	//textFont(arial)
 	//music.volume(0.2);
@@ -50,39 +57,39 @@ var frameTimes = [];
 
 function mouseClicked() {}
 
-var debugScale = false
-var prevPerfTrack = null
+var debugScale = false;
+var prevPerfTrack = null;
 
-var displayedTrack = null
+var displayedTrack = null;
 
 function draw() {
-	PerfTrack.push("Main Loop")
-	PerfTrack.push("Work")
+	PerfTrack.push("Main Loop");
+	PerfTrack.push("Work");
 	background(51);
 	if (debugScale) {
-		scale(0.5)
-		translate(width/2, height/2)
-		push()
-		noFill()
-		stroke(255)
-		rect(0, 0, width, height)
-		pop()
+		scale(0.5);
+		translate(width / 2, height / 2);
+		push();
+		noFill();
+		stroke(255);
+		rect(0, 0, width, height);
+		pop();
 	}
 	//workspace.screenArea.rot += 0.08*deltaTime/1000
 	workspace.update();
 	workspace.draw();
 	PerfTrack.pop();
-	var loop = PerfTrack.pop()
+	var loop = PerfTrack.pop();
 	if (perfGraph && loop) {
 		//loop.endTime = loop.startTime + 16
 		perfGraph.recordTime(loop.getLength());
-		perfGraph.drawGraph(true)
+		perfGraph.drawGraph(true);
 		if (frameCount % 5 == 0 || !displayedTrack) {
-			displayedTrack = loop
+			displayedTrack = loop;
 		}
-		displayedTrack.display(50, height-20, width-100, 0)
+		displayedTrack.display(50, height - 20, width - 100, 0);
 	}
-	prevPerfTrack = loop
+	prevPerfTrack = loop;
 }
 
 function windowResized() {
